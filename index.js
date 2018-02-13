b5bda670396d5fd13bdb504bdf70652399f75286
JP Dillingham <jpdillingham@gmail.com>
2018-02-12 20:01:02 -0600

    Added aws example files

--- index.js
+++
@@ -1,6 +0,0 @@
-import awsServerlessExpress from 'aws-serverless-express';
-import app from './src/app'
-
-const server = awsServerlessExpress.createServer(app, null);
-
-export default (event, context) => awsServerlessExpress.proxy(server, event, context)
