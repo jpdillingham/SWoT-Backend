90449f25809a817539c49b7fc1b7c1d54cc890ed
JP Dillingham <jpdillingham@gmail.com>
2018-03-09 20:43:08 -0600

    Refactored exercise handler

--- exercises.js
+++ exercises.js
@@ -1,26 +1,17 @@
 var data = require('data')
 
 exports.handle = (request, response) => {
+    let path = request.path.toUpperCase().split('/');    
+
     switch(request.httpMethod) {
         case 'GET':
             return get(request, response);
         case 'POST':
-            response.statusCode = 201
-            response.body = request.body
-            return response;
+            return post(request, response);
         case 'PUT':
-            var item = JSON.parse(request.body);
-            var resp = Object.assign({}, item);
-            resp.name = resp.name + '!'
-            
-            response.statusCode = 200
-            response.body = JSON.stringify(resp)
-            return response;
+            return put(request, response);
         case 'DELETE':
-            response.statusCode = 204
-            response.body = event.body
-            response.headers.AssetID = path[2]
-            return response;
+            return _delete(request, response);
         default:
             response.statusCode = 405 // method not available
             return response;
@@ -30,5 +21,28 @@ exports.handle = (request, response) => {
 const get = (request, response) => {
     response.statusCode = 200
     response.body = JSON.stringify(data.exercises)
+    return response;
+}
+
+const post = (request, response) => { 
+    response.statusCode = 201
+    response.body = request.body
+    return response;
+}
+
+const put = (request, response) => {
+    var item = JSON.parse(request.body);
+    var resp = Object.assign({}, item);
+    resp.name = resp.name + '!'
+    
+    response.statusCode = 200
+    response.body = JSON.stringify(resp)
+    return response;
+}
+
+const _delete = (request, response) => {
+    response.statusCode = 204
+    response.body = request.body
+    response.headers.AssetID = path[2]
     return response;
 }
