37f58441c1b89a681742dbd9c642a3b46d9c674f
JP Dillingham <jpdillingham@gmail.com>
2018-03-09 20:52:07 -0600

    Added routines handler

---
+++ routines.js
@@ -0,0 +1,48 @@
+var data = require('data')
+
+exports.handle = (request, response) => {
+    switch(request.httpMethod) {
+        case 'GET':
+            return get(request, response);
+        case 'POST':
+            return post(request, response);
+        case 'PUT':
+            return put(request, response);
+        case 'DELETE':
+            return _delete(request, response);
+        default:
+            response.statusCode = 405 // method not available
+            return response;
+    }
+}
+
+const get = (request, response) => {
+    response.statusCode = 200
+    response.body = JSON.stringify(data.routines)
+    return response;
+}
+
+const post = (request, response) => { 
+    response.statusCode = 201
+    response.body = event.body
+    return response;
+}
+
+const put = (request, response) => {
+    var item = JSON.parse(event.body);
+    var resp = Object.assign({}, item);
+    resp.name = resp.name + '!'
+    
+    response.statusCode = 200
+    response.body = JSON.stringify(resp)
+    return response;
+}
+
+const _delete = (request, response) => {
+    let path = request.path.toUpperCase().split('/');    
+    
+    response.statusCode = 204
+    response.body = event.body
+    response.headers.AssetID = path[2]
+    return response;
+}
