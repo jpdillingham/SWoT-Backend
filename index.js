f5651480a05f781e2c9ad72ad3ad639e77bbfc4e
JP Dillingham <jpdillingham@gmail.com>
2018-03-11 15:17:23 -0500

    Added body-parser

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const awsServerlessExpress = require('aws-serverless-express');   
 const express = require('express');  
 const cors = require('cors');
+const bodyParser = require('body-parser'); 
 
 const app = express();
+
 app.use(cors);
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var data = require('data')
 
