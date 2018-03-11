9e10a0bc9327ebd4093342e47f6efa4dbab52b91
JP Dillingham <jpdillingham@gmail.com>
2018-03-11 14:57:49 -0500

    Added cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const awsServerlessExpress = require('aws-serverless-express');   
 const express = require('express');  
+const cors = require('cors');
 
 const app = express();
+app.use(cors);
 
 var data = require('data')
 
