af2fc0eac176064809f9310e5ab39c5c243395eb
JP Dillingham <jpdillingham@gmail.com>
2018-03-16 18:12:46 -0500

    Added generic get to database.js

--- database.js
+++ database.js
@@ -1,7 +1,21 @@
 const AWS = require('aws-sdk');
 
+const TABLE_NAME = 'SWoT';
+
 AWS.config.update({ region: 'us-east-1' });
 const dynamoDB = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', convertEmptyValues: true });
+
+exports.get = (property, key) => {
+    let params = {
+        TableName: TABLE_NAME,
+        Key: {
+            user: key,
+        },
+        ProjectionExpression: property,
+    };
+    
+    return dynamoDB.get(params).promise();
+}
 
 exports.getRoutines = (key) => {
     let params = {
