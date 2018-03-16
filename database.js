b8fdd644b1016c21d84d1668da7d12c7e22a5fce
JP Dillingham <jpdillingham@gmail.com>
2018-03-15 22:21:21 -0500

    Removed removeEmptyStringElements

--- database.js
+++ database.js
@@ -1,18 +1,7 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({ region: 'us-east-1' });
-const dynamoDB = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-
-const removeEmptyStringElements = (obj) => {
-  for (var prop in obj) {
-    if (typeof obj[prop] === 'object') {// dive deeper in
-      removeEmptyStringElements(obj[prop]);
-    } else if(obj[prop] === '') {// delete elements that are empty strings
-      delete obj[prop];
-    }
-  }
-  return obj;
-}
+const dynamoDB = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', convertEmptyValues: true });
 
 exports.getRoutines = (key) => {
     let params = {
@@ -53,8 +42,6 @@ exports.getExercises = (key) => {
 }
 
 exports.setExercises = (key, exercises) => {
-    removeEmptyStringElements(exercises);
-    
     let params = {
         TableName: 'SWoT',
         Key: { 
