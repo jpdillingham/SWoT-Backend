1d95a17686d4d4f6950d464b212e1169a5e766df
JP Dillingham <jpdillingham@gmail.com>
2018-03-17 18:55:39 -0500

    Implemented build logic

--- tools/zip.py
+++ tools/zip.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 import os
-import zipfile
 import shutil
-
-def zipdir(path, ziph):
-    # ziph is zipfile handle
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            print(os.path.join(root, file))
-            ziph.write(root + "/" + file)
+import tempfile
 
 if __name__ == '__main__':
-    shutil.make_archive('../build/swot.zip', 'zip', '../node_modules')
-    # zipf = zipfile.ZipFile('swot.zip', 'w', zipfile.ZIP_DEFLATED)
-    # zipdir('', zipf)
-    # zipf.close()
+    print('creating deployable zip...')
+
+    temp = os.path.join(tempfile.gettempdir(), 'swot')
+    output = os.path.join('..', 'build', 'build.zip')
+
+    if os.path.exists(output):
+        print('output file exists.  deleting \'' + output + '\'')
+        os.remove(output)
+
+    print ('creating zip in \'' + temp + '\'')
+    shutil.make_archive(temp, 'zip', '../')
+
+    print('copying \'' + temp + '\' to \'' + output + '\'')
+    shutil.move(temp + '.zip', output)
+
+    print('build succeeded.')
