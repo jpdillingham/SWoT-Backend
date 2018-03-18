7f6a7071d784d847545cf42bf72ad6ea9a6f6785
JP Dillingham <jpdillingham@gmail.com>
2018-03-17 21:46:14 -0500

    Final tweaks to zip script

--- tools/zip.py
+++ tools/zip.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
+import sys
 import os
 import shutil
 import tempfile
 import uuid
 
 if __name__ == '__main__':
-    print('creating deployable zip from app directory...')
+    src = os.path.abspath(sys.argv[1])
+    dest = os.path.abspath(sys.argv[2])
+    print('creating deployable zip from ' + src + '...')
 
     temp = os.path.join(tempfile.gettempdir(), 'swot-builds', str(uuid.uuid4()))
-    output = os.path.join('..', 'build', 'deploy.zip')
+    output = os.path.join(dest, 'deploy.zip')
 
     if os.path.exists(output):
         print('output file exists.  deleting \'' + output + '\'')
         os.remove(output)
 
     print ('creating zip in \'' + temp + '\'')
-    shutil.make_archive(temp, 'zip', '../')
+    shutil.make_archive(temp, 'zip', src)
 
     print('copying \'' + temp + '\' to \'' + output + '\'')
     shutil.move(temp + '.zip', output)
