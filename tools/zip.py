e68e066f9e64dadffc27a3bde6c89a9220e129ff
JP Dillingham <jpdillingham@gmail.com>
2018-03-18 10:52:35 -0500

    Added error handling to zip.py

--- tools/zip.py
+++ tools/zip.py
@@ -8,19 +8,28 @@ import uuid
 if __name__ == '__main__':
     src = os.path.abspath(sys.argv[1])
     dest = os.path.abspath(sys.argv[2])
-    print('creating deployable zip from ' + src + '...')
-
     temp = os.path.join(tempfile.gettempdir(), 'swot-builds', str(uuid.uuid4()))
     output = os.path.join(dest, 'deploy.zip')
 
-    if os.path.exists(output):
-        print('output file exists.  deleting \'' + output + '\'')
-        os.remove(output)
+    print('creating deployable zip from ' + src + '...')
 
-    print ('creating zip in \'' + temp + '\'')
-    shutil.make_archive(temp, 'zip', src)
+    try:
+        if os.path.exists(output):
+            print('output file exists.  deleting \'' + output + '\'')
+            os.remove(output)
 
-    print('copying \'' + temp + '\' to \'' + output + '\'')
-    shutil.move(temp + '.zip', output)
+        print ('creating zip in \'' + temp + '\'...')
+        shutil.make_archive(temp, 'zip', src)
 
-    print('zip succeeded.')
+        print('copying \'' + temp + '\' to \'' + output + '\'...')
+        shutil.move(temp + '.zip', output)
+
+        print('zip succeeded.')
+    except:
+        e = sys.exc_info()[0]
+        print('error: ' + e)
+        print('cleaning up...')
+
+        shutil.rmtree(temp)
+
+        print('zip failed.')
