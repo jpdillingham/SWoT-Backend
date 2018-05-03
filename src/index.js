ecefef75ee2d1179ec5548c9f3ab697566bb2770
JP Dillingham <jpdillingham@gmail.com>
2018-05-03 07:20:21 -0500

    for real this time

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const workoutSort = (predicate) => {
         a = a.endTime;
         b = b.endTime;
         
-        if (predicate === 'desc') {
+        if (predicate === 'asc') {
             if (a > b) return 1;
             if (a < b) return -1;
             return 0;
