2a59638ae33e71582b4abd9023a1fb4e8633f1ba
JP Dillingham <jpdillingham@gmail.com>
2018-05-16 22:39:28 -0500

    implement exercise history

--- src/controllers/exercises.js
+++ src/controllers/exercises.js
@@ -4,6 +4,48 @@ const util = require('../util')
 
 const router = express.Router();
 
+// pagination - /workouts?limit=N&offset=M
+// sort - /workouts?order=<ASC|DESC>
+// filter by routine - /workouts?routineId=guid
+// filter by date range = /workouts?fromDate=<unix timestamp>&toDate=<unix timestamp>
+router.get('/history', (req, res) => {
+    let userId = util.getUserId(req);
+    let order = req.query && req.query.order ? req.query.order.toLowerCase() : undefined;
+    let exerciseId = req.query && req.query.exerciseId ? req.query.exerciseId.toLowerCase() : undefined;
+    let limit = req.query && req.query.limit ? req.query.limit : undefined;
+    let offset = req.query && req.query.offset ? req.query.offset : undefined;
+    let fromTime = req.query && req.query.fromTime ? req.query.fromTime : 0;
+    let toTime = req.query && req.query.toTime ? req.query.toTime : new Date().getTime();
+
+    database.queryAll(userId, fromTime, toTime)
+    .then(workouts => {
+        let exercises = workouts
+                        .map(w => w.routine.exercises)
+                        .reduce((acc, e) => acc.concat(e, []))
+
+        if (exerciseId) {
+            exercises = exercises.filter(e => e.id === exerciseId);
+        }
+
+        res.header('X-Total-Count', exercises.length);
+
+        if (order) {
+            // TODO: sort
+        }
+
+        if (offset && limit) {
+            exercises = exercises.slice(+offset, +offset + +limit);
+        }
+
+        res.status(200);
+        res.json(exercises);
+    })
+    .catch(err => {
+        res.status(500);
+        res.json(err);
+    })
+})
+
 router.get('/', (req, res) => {
     let userId = util.getUserId(req);
 
